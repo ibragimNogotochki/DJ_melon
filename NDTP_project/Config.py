c6c399d29f2ec16e160834555e2fc05926cf60e0
ibragimNogotochki <leha.akimenko2007@gmail.com>
2023-05-04 21:09:26 +0300

    music classes written

--- NDTP_project/Config.py
+++ NDTP_project/Config.py
@@ -1,6 +1,6 @@
 import telebot
 import lyricsgenius
 
-bot = telebot.TeleBot('6157578435:AAEnF4_LW2PD0utagpOLsv4jXNqwI_wX93c')
-genius = lyricsgenius.Genius('S8rbZ1tqGA6gVNAET-_jZG_vDNxPPr0Y_k7q_MnZ_xwxJ_T6rVugK1C-su6FuiGw')
+bot = telebot.TeleBot('')
+genius = lyricsgenius.Genius('')
 types = telebot.types
